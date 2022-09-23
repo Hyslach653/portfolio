45b62a703a679b26f9bc799334a456f6b3ab743f
Hyslach653 <tonies395@gmail.com>
2022-09-23 12:40:00 +0200

    testing tests

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,26 +1,26 @@
 /** @jsxRuntime classic */
 /* @jsx jsx */
-import { jsx } from '@emotion/react';
+import { jsx } from '@emotion/react'
 
-
-export default function Home() {
+export default function Home () {
   return (
     <div css={homeStyle}>
       {/* if hover => show actual image ?? */}
-      <img className='profileGif' alt="profile" src="/pictures/profile.gif" />
+      <img className='profileGif' alt='profile' src='/pictures/profile.gif' />
+      <p>Learn react</p>
     </div>
   )
 }
 
 const homeStyle = {
-  minHeight: "200vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  paddingTop: "10vh",
-  ".profileGif": {
-    borderRadius: "100%",
+  minHeight: '200vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  paddingTop: '10vh',
+  '.profileGif': {
+    borderRadius: '100%',
     // make width responsive!
-    width: "25vw"
+    width: '25vw'
   }
 }
